dbb66d73db9715258e42fa18c7fec598d1e889ef
pytdata <sampahe13@outlook.com>
2026-04-30 13:37:50 +0000

    updated analyzer DB_URL text

--- integration/listener_service/db.py
+++ integration/listener_service/db.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
-engine = create_async_engine(settings.DBURL, echo=False, pool_pre_ping=True)
+engine = create_async_engine(settings.DB_URL, echo=False, pool_pre_ping=True)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
